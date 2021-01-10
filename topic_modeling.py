7b03f3da7027d56153f0fed26195e139b74badbb
satrfall6 <satrfall6@gmail.com>
2021-01-10 20:40:11 +1000

    ADD: func to cal r and s for corpus

--- topic_modeling.py
+++ topic_modeling.py
@@ -4,7 +4,7 @@ Created on Wed Jan  6 14:42:39 2021
 
 @author: satrf
 """
-
+import tqdm
 import gensim
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -27,7 +27,43 @@ def convert_to_nlp_format(final_vw_dict, total_regions):
             corpus_dict[key].append(temp_doc)
     return corpus_dict
 
-def hdp_modeling(region_corpus, model_type, total_words, num_topics = 100):
+def cal_region_r_and_s(corpus_dict, total_regions):
+    '''
+    Objective: this is to calculate reconstruction and score for the base distribution
+                different from "cal_clip_score"
+    input:
+        
+    output: 
+        reconstruction_dict: a dictionary of arrays, stores reconstructions for IPs
+        score_dict: a 
+    '''
+    reconstruction_dict = dict((k, np.array([])) for k in range(total_regions))  
+    score_dict = dict((k, np.array([])) for k in range(total_regions))
+    for rgs in tqdm.tqdm(corpus_dict.keys()):
+        
+        if len(corpus_dict[rgs]) >0:
+    
+            lda_model = gensim.models.LdaModel(corpus=corpus_dict[rgs], id2word=None, 
+                                           num_topics = 150) 
+            r_region = np.array([])
+            for doc in corpus_dict[rgs]:
+                r = cal_ip_reconstruction(doc, lda_model)
+                if len(r_region) <1:
+                    r_region = r
+                else:
+                    r_region = np.r_[r_region, r]
+            reconstruction_dict[rgs] = r_region   
+            
+            s_region = []
+            r_tr = lda_model.get_topics()
+            for r_j in reconstruction_dict[rgs]:
+                s_region.append(cal_confidence_score(r_j.reshape(1,-1), r_tr))
+        
+            score_dict[rgs] = s_region  
+    return reconstruction_dict, score_dict
+
+def topic_modeling(corpus_dict, model_type, total_regions, 
+                   total_words = 1153, num_topics = 150):
     '''
     Objective: do "lda" and "hdp" modeling 
     input:
@@ -40,30 +76,49 @@ def hdp_modeling(region_corpus, model_type, total_words, num_topics = 100):
         mask[..., 0]: it's actually the angle, for building the descriptor
         rgb: for visualizing the flow, not use at this moment
     '''
-    if model_type == 'lda':
-        lda_model = gensim.models.LdaModel(corpus=region_corpus, id2word=None, 
-                                   num_topics = num_topics)
-        return lda_model
-    elif model_type == 'hdp':
-        region_dict = [str(i) for i in range(total_words +1)] 
-        Hdp_model = gensim.models.hdpmodel.HdpModel(corpus=region_corpus, id2word=region_dict)
-        return Hdp_model
-#    else: 
+    model_dict = dict((k, None) for k in range(total_regions))
+    for rgs in corpus_dict.keys(): 
+        if len(corpus_dict[rgs]) > 0:          
+            if model_type == 'lda':
+                lda_model = gensim.models.LdaModel(corpus=corpus_dict[rgs], id2word=None, 
+                                           num_topics = num_topics)
+                model_dict[rgs] = lda_model
+            elif model_type == 'hdp':
+                region_dict = [str(i) for i in range(total_words)] 
+                hdp_model = gensim.models.hdpmodel.HdpModel(corpus=corpus_dict[rgs], 
+                                                            id2word=region_dict)
+                model_dict[rgs] = hdp_model
+            else: 
+                print("Not an available model type!")
+                return None
+    return model_dict
 
-def cal_region_reconstruction(doc, model):
+def cal_ip_reconstruction(doc, model):
     '''
     Objective: calculate equation(5) for an IP 
     input:
-        doc: a document in BOW format (1153-vector)
+        doc: a document in (word_id, word_cound) format 
         model: gensim model function, either 'lda' or 'hdp'
     Output:
-        r_d: reconstruction of "an IP" in a clip
+        r_d: reconstruction of "an IP" in a clip (IP in a region)
+        (A region might contains multiple IPs)
     '''
     # the total topics 
-    document_topics = model.get_document_topics(doc, minimum_probability = 0.005)
+    try:
+        document_topics = model.get_document_topics(doc, minimum_probability = 0.005)
+    except:
+        document_topics = [t for t in model.__getitem__(doc) if t[1] > 0.005]
+
     r_d = np.array([])    
     for topic in document_topics:
-        topic_terms = model.get_topic_terms(topic[0])
+        try:
+            topic_terms = model.get_topic_terms(topic[0])
+        except:
+            topic_terms_dist = model.get_topics()[topic[0]]
+            over_thres = topic_terms_dist[np.where(topic_terms_dist>0.0035)].shape[0]
+            top_words = topic_terms_dist.argsort()[-over_thres:][::-1]
+            top_words_prob = topic_terms_dist[topic_terms_dist.argsort()[-over_thres:][::-1]]
+            topic_terms = [(top_words[i], top_words_prob[i]) for i in range(over_thres)]
         r = topic_terms_to_array(topic_terms) * topic[1]
         if len(r_d) < 1 :
             r_d = r
@@ -72,7 +127,7 @@ def cal_region_reconstruction(doc, model):
             
     return r_d
  
-def topic_terms_to_array(topic_terms, total_words):
+def topic_terms_to_array(topic_terms, total_words=1153):
     '''
     Objective: topic terms from gensim model is in the format [(word_id, word_prob)...]
                 in order to sum the topic terms, convert to array
@@ -81,7 +136,7 @@ def topic_terms_to_array(topic_terms, total_words):
     Output:
         [0.2, 0.7, 0, 0.05, ...]
     '''
-    terms_array = np.zeros([1, total_words +1])
+    terms_array = np.zeros([1, total_words])
     for terms in topic_terms:
         terms_array[0, int(terms[0])] += terms[1]
     return terms_array
